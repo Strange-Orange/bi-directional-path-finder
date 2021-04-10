eb373f75b807bf4908cbe05e8616a37eab92ae57
Calum Macdonald <calum.macdonald246@gmail.com>
2021-04-10 15:09:08 +0100

    Add destination to edge

--- src/edge.h
+++ src/edge.h
@@ -11,6 +11,8 @@ struct Edge
 {
     // Cost will be distance as the crow flies, model as if it were flat. May change this later
     double m_cost;
+    int m_destLat;
+    int m_destLng;
     std::string m_dest;
 
     Edge()
@@ -22,6 +24,8 @@ struct Edge
         double l_x = abs(p_end.get_lng() - p_start.get_lng());
         double l_y = abs(p_end.get_lat() - p_start.get_lat());
         m_cost = sqrt((l_x * l_x) + (l_y * l_y));
+        m_destLat = p_end.get_lat();
+        m_destLng = p_end.get_lng();
         m_dest = p_end.get_name();
     }
 };
