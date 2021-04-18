c0c74915543d9b28f983bcf2d54e122b0e6cca1b
Calum Macdonald <calum.macdonald246@gmail.com>
2021-04-18 18:48:00 +0100

    Creates red path to show route taken
    Currently using standard Dijkstra search

--- src/common.h
+++ src/common.h
@@ -5,15 +5,29 @@
 #include "edge.h"
 
 #include <unordered_map>
+#include <unordered_set>
 #include <vector>
 #include <SDL2/SDL.h>
 
 typedef std::unordered_map<Vertex, std::vector<Edge>, VertexHash, VertexCompare> adjacencyList;
+typedef std::unordered_map<Vertex, Vertex, VertexHash, VertexCompare> previousVertexMap;
 
 const int WIDTH = 1024;
 const int HEIGHT = 1024;
 
 extern SDL_Window* g_window;
 extern SDL_Renderer* g_renderer;
+
+struct StartEnd
+{
+    Vertex m_start;
+    Vertex m_end;
+};
+
+struct RouteData
+{
+    std::unordered_set<Vertex, VertexHash, VertexCompare> m_vertices;
+    std::unordered_set<Edge, EdgeHash, EdgeCompare> m_edges;
+};
 
 #endif
--- src/main.cc
+++ src/main.cc
@@ -6,17 +6,22 @@
 
 #include <iostream>
 #include <string>
+#include <unordered_map>
 #include <unordered_set>
 #include <utility>
 #include <SDL2/SDL.h>
+
+#include <limits.h>
 
 SDL_Window* g_window = nullptr;
 SDL_Renderer* g_renderer = nullptr;
 
 bool init();
 void quit();
-inline void lat_lng_position(const Vertex& p_v, const Bounds& p_bounds, int& o_x, int& o_y);
-
+inline void lat_lng_position(int p_lat, int p_lng, const Bounds& p_bounds, int& o_x, int& o_y);
+StartEnd place_name_to_vertex(const std::string& p_start, const std::string& p_end, const adjacencyList& p_adj);
+void get_edges_in_route(RouteData& p_rd, const Vertex& p_start, const Vertex& p_end, const previousVertexMap& p_prevMap, const adjacencyList& p_adj);
+RouteData bi_directional_dijkstra(const adjacencyList& p_adj, const Vertex& p_start, const Vertex& p_end);
 // Initialize SDL and create window and renderer
 bool init()
 {
@@ -59,15 +64,126 @@ void quit()
     SDL_Quit();
 }
 
-// FIX: Does not work for areas that are not in the north east, all locations have position latitude and longitude
-inline void lat_lng_position(const Vertex& p_v, const Bounds& p_bounds, int& o_x, int& o_y)
+inline void lat_lng_position(int p_lat, int p_lng, const Bounds& p_bounds, int& o_x, int& o_y)
 {
     int l_xrange = p_bounds.m_east - p_bounds.m_west;
     int l_yrange = p_bounds.m_north - p_bounds.m_south;
-    o_x = ((p_v.get_lng() - p_bounds.m_west) * (HEIGHT - 5)) / l_xrange; 
-    o_y = ((p_v.get_lat() - p_bounds.m_south) * (WIDTH - 5)) / l_yrange;
+    o_x = ((p_lng - p_bounds.m_west) * (HEIGHT - 5)) / l_xrange; 
+    o_y = ((p_lat - p_bounds.m_south) * (WIDTH - 5)) / l_yrange;
     // Flip the y axis otherwise the graph will be upside down
     o_y = (HEIGHT - 5) - o_y;
+}
+
+StartEnd place_name_to_vertex(const std::string& p_start, const std::string& p_end, const adjacencyList& p_adj)
+{
+    StartEnd l_se;
+    bool l_startFound = false, l_endFound = false;
+    for (const auto& l_item: p_adj)
+    {
+        if (l_item.first.get_name_c() == p_start)
+        {
+            l_se.m_start = l_item.first;
+            l_startFound = true;
+        }
+        else if (l_item.first.get_name_c() == p_end)
+        {
+            l_se.m_end = l_item.first;
+            l_endFound = true;
+        }
+        else
+            continue;
+
+        if (l_startFound && l_endFound)
+            break;
+    }
+
+    return l_se;
+}
+
+// Collect all the edges used in the route to the end point
+void get_edges_in_route(RouteData& p_rd, const Vertex& p_start, const Vertex& p_end, const previousVertexMap& p_prevMap, const adjacencyList& p_adj)
+{
+    Vertex l_current = p_end;
+    while (l_current != p_start)
+    {
+        const Vertex& l_prev = p_prevMap.at(l_current);
+        for (const auto& l_edge: p_adj.at(l_prev))
+        {
+            // Found the edge on the route
+            if (l_edge.m_dest == l_current.get_name_c())
+            {
+                p_rd.m_edges.insert(l_edge);
+                break;
+            }
+        }
+        l_current = l_prev;
+    }
+}
+
+RouteData bi_directional_dijkstra(const adjacencyList& p_adj, const Vertex& p_start, const Vertex& p_end)
+{
+    // Initialize visited, distance and previous maps
+    std::unordered_map<Vertex, bool, VertexHash, VertexCompare> l_visited;
+    std::unordered_map<Vertex, int, VertexHash, VertexCompare> l_dist;
+    previousVertexMap l_prev;
+
+    for (const auto& l_item: p_adj)
+    {
+        l_visited[l_item.first] = false;
+        l_dist[l_item.first] = INT_MAX;
+        l_prev[l_item.first];
+    }
+
+    l_dist.at(p_start) = 0;
+    IndexedPriorityQueue<Vertex, VertexHash> l_pq;
+    l_pq.insert(p_start);
+
+    while (l_pq.size() != 0 && !l_visited.at(p_end))
+    {
+        Vertex l_current = l_pq.extract_min();
+        l_visited.at(l_current) = true;
+        // std::cout << "Current: " << l_current.get_name_c() << "\n";
+        // View all the connected vertices
+        int l_edgeIndex = 0;
+        for (const auto& l_edge: p_adj.at(l_current))
+        {
+            Vertex l_viewing(l_edge.m_destLat, l_edge.m_destLng, l_edge.m_dest);
+            // If the viewing vertex has already been visited just ignore it
+            if (!l_visited.at(l_viewing))
+            {
+                // std::cout << "viewing : " << l_viewing.get_name_c() << ": Distance: ";
+                int l_newDist = l_dist.at(l_current) + l_edge.m_cost;
+                // Relax the edge
+                if (l_newDist < l_dist.at(l_viewing))
+                {
+                    // std::cout << l_newDist << "\n";
+                    l_prev.at(l_viewing) = l_current;
+                    // Edge to the viewing vertex
+                    l_dist.at(l_viewing) = l_newDist;
+                    // Update the priority queue
+                    Vertex l_updated(l_viewing);
+                    l_pq.insert(l_updated);
+                    l_updated.set_cost(l_newDist);
+                    l_pq.change_priority(l_viewing, l_updated);
+                }
+            }
+            l_edgeIndex++;
+        }
+    }
+
+    RouteData l_rd;
+    if (l_visited.at(p_end))
+    {
+        Vertex l_viewing = p_end;
+        while (l_viewing.get_name_c() != p_start.get_name_c())
+        {
+            l_rd.m_vertices.insert(l_viewing);
+            l_viewing = l_prev.at(l_viewing);
+        }
+        get_edges_in_route(l_rd, p_start, p_end, l_prev, p_adj);
+        return l_rd;
+    }
+    return l_rd;
 }
 
 int main(int argc, char* args[])
@@ -82,22 +198,20 @@ int main(int argc, char* args[])
     else
         l_adj = parse_data_csv("./data.csv");
 
-    std::vector<SDL_Rect> l_cityRects(l_adj.size());
+    StartEnd l_se = place_name_to_vertex("Beijing", "Shenzhen", l_adj);
+    RouteData l_route = bi_directional_dijkstra(l_adj, l_se.m_start, l_se.m_end);
+    for (const Vertex& l_cv: l_route.m_vertices)
+        std::cout << l_cv.get_name() << " : ";
+    std::cout << std::endl;
+
+    std::unordered_set<Edge, EdgeHash, EdgeCompare> l_routeEdges;
+
+    std::vector<SDL_Rect> l_cityRects(l_adj.size() - l_route.m_vertices.size());
+    std::vector<SDL_Rect> l_routeRects(l_route.m_vertices.size());
     Bounds geoBounds = get_bounds();
-    std::cout << geoBounds.m_east << " : " << geoBounds.m_west << " : " << geoBounds.m_south << " : " << geoBounds.m_north << "\n";
 
     bool l_quit = false;
     SDL_Event l_event;
-
-    for (const std::pair<Vertex, std::vector<Edge>>& l_item: l_adj)
-    {
-        std::cout << "Location: " << l_item.first.get_name() << " : " << l_item.first.get_lat() << " : " << l_item.first.get_lng() << "\n";
-        for (const Edge& l_e: l_item.second)
-        {
-            std::cout << l_e.m_dest << " : ";
-        }
-        std::cout << std::endl;
-    }
 
     // Main loop
     while (!l_quit)
@@ -122,22 +236,37 @@ int main(int argc, char* args[])
         SDL_RenderClear(g_renderer);
 
         SDL_SetRenderDrawColor(g_renderer, 0, 0, 255, 255);
+        std::unordered_set<Edge, EdgeHash, EdgeCompare> l_edges;
         int l_vIndex = 0;
+        int l_vRouteIndex = 0;
         for (const std::pair<Vertex, std::vector<Edge>>& l_item: l_adj)
         {
             int l_x, l_y;
-            lat_lng_position(l_item.first, geoBounds, l_x, l_y);
-            l_cityRects.at(l_vIndex++) = {l_x, l_y, 5, 5};
+            lat_lng_position(l_item.first.get_lat(), l_item.first.get_lng(), geoBounds, l_x, l_y);
+            if (l_route.m_vertices.find(l_item.first) == l_route.m_vertices.end())
+                l_cityRects.at(l_vIndex++) = {l_x, l_y, 5, 5};
+            else
+                l_routeRects.at(l_vRouteIndex++) = {l_x, l_y, 5, 5};
 
             for (const Edge& l_e: l_item.second)
             {
-                int l_dx, l_dy;
-                Vertex l_dest = geoBounds.m_cities.at(l_e.m_dest);
-                lat_lng_position(l_dest, geoBounds, l_dx, l_dy);
-                SDL_RenderDrawLine(g_renderer, l_x, l_y, l_dx, l_dy);
+                if (l_edges.find(l_e) == l_edges.end())
+                {
+                    l_edges.insert(l_e);
+                    int l_dx, l_dy;
+                    lat_lng_position(l_e.m_destLat, l_e.m_destLng, geoBounds, l_dx, l_dy);
+                    // Change draw colour if the edge is part of the route
+                    if (l_route.m_edges.find(l_e) != l_route.m_edges.end())
+                        SDL_SetRenderDrawColor(g_renderer, 255, 0, 0, 255);
+                    else
+                        SDL_SetRenderDrawColor(g_renderer, 0, 0, 255, 255);
+                    SDL_RenderDrawLine(g_renderer, l_x, l_y, l_dx, l_dy);
+                }
             }
         }
         SDL_RenderFillRects(g_renderer, l_cityRects.data(), l_cityRects.size());
+        SDL_SetRenderDrawColor(g_renderer, 255, 0, 0, 255);
+        SDL_RenderFillRects(g_renderer, l_routeRects.data(), l_routeRects.size());
         
         SDL_RenderPresent(g_renderer);
     } 
